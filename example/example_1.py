c4d83e221d2c772755c6063bb5dcc695c1c00e6a
Happy2018new <chenbo79800@163.com>
2023-04-30 15:02:07 +0800

    bug fix on `simple_1.py`

--- example/example_1.py
+++ example/example_1.py
@@ -1,6 +1,6 @@
-from BDXConverter.Converter import ReadBDXFile, ConvertListIntoJSONFile
+from BDXConverter.Converter import ReadBDXFile, VisualStructs
 
 # 将当前目录下的 test.bdx 标准化为 ans.json
 # ans.json 将生成在当前目录下
-readResult, _ = ReadBDXFile('test.bdx')
-ConvertListIntoJSONFile(readResult, 'ans.json')
+readResult, _ = ReadBDXFile('test.bdx')  # 读取 test.bdx
+VisualStructs(readResult, 'ans.json')  # 标准化为 JSON
