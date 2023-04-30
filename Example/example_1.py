2c53bf3acd1cba633c8ef98d413741e82558034c
Happy2018new <chenbo79800@163.com>
2023-04-30 08:49:51 +0800

    change the first letter

--- Example/example_1.py
+++
@@ -1,5 +0,0 @@
-from BDXConverter.Converter import ReadBDXFile, ConvertListIntoJSONFile
-
-# 将当前目录下的 test.bdx 标准化为 ans.json
-# ans.json 将生成在当前目录下
-ConvertListIntoJSONFile(ReadBDXFile('test.bdx'), 'ans.json')
